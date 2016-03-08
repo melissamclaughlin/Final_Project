9f08bf6ab310dd8aac0e28b4929dafcf1abc07f7
melissamclaughlin <melissamclaughlindesigns@gmail.com>
2016-03-08 16:05:09 -0500

    Added age verification

--- JS/main.js
+++ JS/main.js
@@ -22,3 +22,11 @@
 });
 
 
+var userAge = parseInt(prompt("What's your age?"),10);
+if (isNaN(userAge)) {
+	console.log("It looks like you didn't enter a number.");
+} else if (userAge < 21) {
+	console.log("Sorry, you have to be 21 or older to use this website!");
+} else {
+	console.log("You're " + userAge + " years old? Go ahead!");
+}
